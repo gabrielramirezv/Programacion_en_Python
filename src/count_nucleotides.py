4ec65244e9d9e80dbc2d5fc6863c07753e167195
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-03-17 12:29:28 -0600

    Se agrega script count_nucleotides.py

---
+++ src/count_nucleotides.py
@@ -0,0 +1,3 @@
+dna = input("Ingresa una secuencia de DNA:\n")
+
+print(f"Frecuencia de nucleotidos \n A = {dna.count('A')} \n T = {dna.count('T')} \n C = {dna.count('C')} \n G = {dna.count('G')}")git 
