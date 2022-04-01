9e6ecae96484460be7c5616e734b008f8050d151
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-03-31 20:02:12 -0600

    Se agrega código funcional para conteo de nucleótidos

--- src/nucleotide-count.py
+++ src/nucleotide-count.py
@@ -3,7 +3,7 @@ NAME
     nucleotide-count
 
 VERSION
-    0.2
+    1.0
 
 AUTHOR
     Gabriel Ramirez Vilchis
@@ -12,7 +12,7 @@ GITHUB
     https://github.com/gabrielramirezv/Programacion_en_Python/blob/master/src/nucleotide-count.py
 
 DESCRIPTION
-    Calcula la frecuencia de A, T, G y C en una secuencia de DNA
+    Calcula la frecuencia de A, T, C y G en una secuencia de DNA
 
 CATEGORY
     DNA sequence
@@ -27,7 +27,7 @@ INPUT
     Secuencia de DNA desde teclado
 
 OUTPUT
-    Frecuencia de A, T, G y C en la secuencia
+    Frecuencia de A, T, C y G en la secuencia
 
 SEE ALSO
     None
@@ -37,9 +37,15 @@ SEE ALSO
 ## 1. Inicio
 
 ## 2. Leer secuencia de DNA desde teclado
+dna_sequence = input("Introduce la secuencia de DNA: ")
 
-## 3. Contar el total de A, T, G y C en la secuencia
+## 3. Contar el total de A, T, C y G en la secuencia
+a_count = dna_sequence.count('A')
+t_count = dna_sequence.count('T')
+c_count = dna_sequence.count('C')
+g_count = dna_sequence.count('G')
 
 ## 4. Imprimir resultados
+print(f"El total por base es:    A: {a_count}  T: {t_count}  C: {c_count}  G: {g_count}")
 
 ## 5. Fin
