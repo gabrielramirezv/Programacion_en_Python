05ac8416479e2962f2d3cc1b56ac7030fb1b1d3e
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-04-01 17:17:12 -0600

    Se agrega algoritmo para formatear secuencia de DNA a FASTA

---
+++ src/dna-fasta.py
@@ -0,0 +1,45 @@
+'''
+NAME
+    dna-fasta
+
+VERSION
+    0.1
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    
+
+DESCRIPTION
+    Dado un archivo dna.txt que contiene una secuencia de DNA, genera
+    un nuevo archivo con la misma secuencia en formato FASTA.
+
+CATEGORY
+    DNA sequence
+
+USAGE
+    py dna-fasta.py
+
+ARGUMENTS
+    None
+
+INPUT
+    Archivo data/dna.txt que contiene una secuencia de DNA sin formato
+
+OUTPUT
+    Archivo nuevo results/dna.fasta con la secuencia en formato FASTA
+    Nota: El nuevo archivo dna.fasta se guarda en la carpeta results/
+
+SEE ALSO
+    None
+
+'''
+
+## 1. Inicio
+
+## 2. Abrir el archivo, leer el contenido y cerrar el archivo
+
+## 3. Abrir nuevo archivo, escribir secuencia en FASTA y cerrar archivo
+
+## 4. Fin
