4ac82e41b7694913a231b6c8877019748d887f38
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-03-31 23:00:42 -0600

    Se agrega algoritmo para cálculo de porcentaje de AT y GC

---
+++ src/AT_GC_percentage.py
@@ -0,0 +1,49 @@
+'''
+NAME
+    AT_GC_percentage
+
+VERSION
+    0.1
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    
+
+DESCRIPTION
+    Calcula el porcentaje de AT y GC de una secuencia de DNA
+
+CATEGORY
+    DNA sequence
+
+USAGE
+    py AT_GC_percentage.py
+
+ARGUMENTS
+    None
+
+INPUT
+    Ruta y nombre de un archivo que contiene una secuencia de DNA
+
+OUTPUT
+    Porcentaje de AT y GC en la secuencia de DNA
+
+SEE ALSO
+    None
+
+'''
+
+## 1. Inicio
+
+## 2. Leer desde teclado la ruta y nombre del archivo con la secuencia
+
+## 3. Abrir el archivo, leer el contenido y cerrar el archivo
+
+## 4. Eliminar caracteres ajenos a la secuencia de DNA
+
+## 5. Calcular porcentaje de AT y GC en la secuencia de DNA
+
+## 6. Imprimir resultados
+
+## 7. Fin
