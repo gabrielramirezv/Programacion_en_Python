a7d1da5b00440f235b2cfa797b8f29ec745787e8
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-03-31 19:33:19 -0600

    Se agrega algoritmo para conteo de nucleótidos en una secuencia de DNA

---
+++ src/nucleotide-count.py
@@ -0,0 +1,45 @@
+'''
+NAME
+    nucleotide-count
+
+VERSION
+    0.1
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    
+
+DESCRIPTION
+    Calcula la frecuencia de A, T, G y C en una secuencia de DNA
+
+CATEGORY
+    DNA sequence
+
+USAGE
+    py nucleotide-count.py
+
+ARGUMENTS
+    None
+
+INPUT
+    Secuencia de DNA desde teclado
+
+OUTPUT
+    Frecuencia de A, T, G y C en la secuencia
+
+SEE ALSO
+    None
+
+'''
+
+## 1. Inicio
+
+## 2. Leer secuencia de DNA desde teclado
+
+## 3. Contar el total de A, T, G y C en la secuencia
+
+## 4. Imprimir resultados
+
+## 5. Fin
