c9d06478b23e0ec3d8b5755b9bdd965aff9b8d4c
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-03-31 23:54:26 -0600

    Se agrega código preliminar para lectura de archivo y conteos

--- src/AT_GC_percentage.py
+++ src/AT_GC_percentage.py
@@ -3,7 +3,7 @@ NAME
     AT_GC_percentage
 
 VERSION
-    0.3
+    0.4
 
 AUTHOR
     Gabriel Ramirez Vilchis
@@ -37,12 +37,22 @@ SEE ALSO
 ## 1. Inicio
 
 ## 2. Leer desde teclado la ruta y nombre del archivo con la secuencia
+dna_file_name = input("Archivo de secuencia: ")
 
 ## 3. Abrir el archivo, leer el contenido y cerrar el archivo
+dna_file = open(dna_file_name, 'r')
+dna_sequence = dna_file_name.read()
+dna_file.close()
 
 ## 4. Eliminar caracteres ajenos a la secuencia de DNA
+dna_sequence.rstrip('\n')
 
 ## 5. Contar A, T, G, C y la longitud de la secuencia
+a_count = dna_sequence.count('A')
+t_count = dna_sequence.count('T')
+c_count = dna_sequence.count('C')
+g_count = dna_sequence.count('G')
+dna_length = dna_sequence = len(dna_sequence)
 
 ## 6. Calcular porcentaje de AT y GC en la secuencia de DNA
 
