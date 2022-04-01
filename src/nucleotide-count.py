15b27a0f7d850bfd987d7913e4adda44bbb0075f
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-03-31 20:25:36 -0600

    Se adapta formato del algoritmo a comentarios del código

--- src/nucleotide-count.py
+++ src/nucleotide-count.py
@@ -34,18 +34,14 @@ SEE ALSO
 
 '''
 
-## 1. Inicio
-
-## 2. Leer secuencia de DNA desde teclado
+# Leer secuencia de DNA desde teclado
 dna_sequence = input("Introduce la secuencia de DNA: ")
 
-## 3. Contar el total de A, T, C y G en la secuencia
+# Contar el total de A, T, C y G en la secuencia
 a_count = dna_sequence.count('A')
 t_count = dna_sequence.count('T')
 c_count = dna_sequence.count('C')
 g_count = dna_sequence.count('G')
 
-## 4. Imprimir resultados
+# Imprimir resultados
 print(f"El total por base es:    A: {a_count}  T: {t_count}  C: {c_count}  G: {g_count}")
-
-## 5. Fin
