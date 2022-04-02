fcf2fea92af4bf818925cdb297114a37e378be1e
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-04-01 22:57:47 -0600

    Se adapta formato del algoritmo a comentarios del código

--- src/sequence-trimming.py
+++ src/sequence-trimming.py
@@ -37,15 +37,13 @@ SEE ALSO
 
 '''
 
-## 1. Inicio
-
-## 2. Abrir archivo, guardar secuencias en una lista y cerrar archivo
+# Abrir archivo, guardar secuencias en una lista y cerrar archivo
 sequences_file = open("data/4_input_adapters.txt", 'r')
 sequences_list = sequences_file.readlines()
 sequences_file.close()
 
-## 3. Abrir nuevo archivo, escribir cada secuencia sin adaptadores y 
-##    cerrar archivo
+# Abrir nuevo archivo, escribir cada secuencia sin adaptadores y 
+# cerrar archivo
 no_adapters_file = open("results/4_output_no_adapters.txt", 'a')
 
 for sequence in sequences_list:
@@ -54,8 +52,6 @@ for sequence in sequences_list:
 
 no_adapters_file.close()
 
-## 4. Informar al usuario que el archivo sin adaptadores se ha creado
+# Informar al usuario que el archivo sin adaptadores se ha creado
 print("\nSe ha generado el archivo 4_output_no_adapters.txt \
       \nEl archivo se encuentra disponible en la carpeta results/ \n")
-
-## 5. Fin
