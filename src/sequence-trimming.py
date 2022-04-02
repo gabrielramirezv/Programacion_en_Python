eab8fc83079695561d4e3cca19963f25ecd468b9
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-04-01 22:51:09 -0600

    Se agrega código funcional para cortar adaptadores

--- src/sequence-trimming.py
+++ src/sequence-trimming.py
@@ -3,7 +3,7 @@ NAME
     sequence-trimming
 
 VERSION
-    0.2
+    1.0
 
 AUTHOR
     Gabriel Ramirez Vilchis
@@ -40,10 +40,22 @@ SEE ALSO
 ## 1. Inicio
 
 ## 2. Abrir archivo, guardar secuencias en una lista y cerrar archivo
+sequences_file = open("data/4_input_adapters.txt", 'r')
+sequences_list = sequences_file.readlines()
+sequences_file.close()
 
 ## 3. Abrir nuevo archivo, escribir cada secuencia sin adaptadores y 
 ##    cerrar archivo
+no_adapters_file = open("results/4_output_no_adapters.txt", 'a')
+
+for sequence in sequences_list:
+    no_adapters_sequence = sequence[14:]
+    no_adapters_file.write(no_adapters_sequence)
+
+no_adapters_file.close()
 
 ## 4. Informar al usuario que el archivo sin adaptadores se ha creado
+print("\nSe ha generado el archivo 4_output_no_adapters.txt \
+      \nEl archivo se encuentra disponible en la carpeta results/ \n")
 
 ## 5. Fin
