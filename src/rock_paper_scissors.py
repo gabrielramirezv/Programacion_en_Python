e4b877b2badf2e83d568af5a3bf547e2fb5772bd
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-04-08 22:13:16 -0500

    Se adapta formato del algoritmo a comentarios del código

--- src/rock_paper_scissors.py
+++ src/rock_paper_scissors.py
@@ -35,68 +35,56 @@ SEE ALSO
 
 '''
 
-# 1. Inicio
 import random
 
-# 2. Definir opciones validas
+# Definir opciones validas
 possible_choices = ["rock", "paper", "scissors"]
 
-# 3. Leer eleccion del usuario desde teclado
+# Leer eleccion del usuario desde teclado y estandarizar a minusculas
 user_choice = input("\nEnter a choice (rock, paper, scissors): ")
-
-# 4. Estandarizar formato del texto de la eleccion del usuario
 user_choice = user_choice.lower()
 
-# 5. Si la eleccion del usuario no es valida, entonces
+# Si la eleccion del usuario no es valida, indicar error y terminar
 if user_choice not in possible_choices:
-    # Imprimir mensaje de error y terminar el programa
     print("\nError: The choice is not valid.\n")
     exit()
 
-# 6. Calcular eleccion de la computadora
+# Calcular eleccion de la computadora
 computer_choice = random.choice(possible_choices)
 
-# 7. Imprimir eleccion de usuario y de computadora
-print(f"\nUser chose {user_choice}, computer chose {computer_choice}.\n")
+# Imprimir eleccion de usuario y de computadora
+print(f"\nUser chose {user_choice}, computer chose {computer_choice}\n")
 
-# 8. Comparar eleccion del usuario con eleccion de la computadora
 
-# 8.1. Si la eleccion del usuario y de la computadora son iguales
+# Comparar
+
+# Si la eleccion del usuario y de la computadora son iguales, es empate
 if user_choice == computer_choice:
-    # Imprimir "Tie"
     print("Tie!\n")
 
-# 8.2. Si la eleccion del usuario es "rock", entonces
+# Si la eleccion del usuario es "rock", evaluar eleccion de computadora
 elif user_choice == "rock":
-    # 8.2.1. Si la eleccion de la computadora es "paper", entonces
+    # Si la eleccion de la computadora es "paper", computadora gana
     if computer_choice == "paper":
-        # Imprimir "Computer wins!"
         print("Computer wins!\n")
-    # 8.2.2. Si la eleccion de la computadora es "scissors", entonces
+    # Si la eleccion de la computadora es "scissors", usuario gana
     else:
-        # Imprimir "User wins!"
         print("User wins!\n")
 
-# 8.3. Si la eleccion del usuario es "paper", entonces
+# Si la eleccion del usuario es "paper", evaluar eleccion de computadora
 elif user_choice == "paper":
-    # 8.3.1. Si la eleccion de la computadora es "rock", entonces
+    # Si la eleccion de la computadora es "rock", usuario gana
     if computer_choice == "rock":
-        # Imprimir "User wins!"
         print("User wins!\n")
-    # 8.3.2. Si la eleccion de la computadora es "scissors", entonces
+    # Si la eleccion de la computadora es "scissors", computadora gana
     else:
-        # Imprimir "Computer wins!"
         print("Computer wins!\n")
 
-# 8.4. Si la eleccion del usuario es "scissors", entonces
+# Si eleccion del usuario es "scissors", evaluar eleccion de computadora
 else:
-    # 8.4.1. Si la eleccion de la computadora es "rock", entonces
+    # Si la eleccion de la computadora es "rock", computadora gana
     if computer_choice == "rock":
-        # Imprimir "Computer wins!"
         print("Computer wins!\n")
-    # 8.4.2. Si la eleccion de la computadora es "paper", entonces
+    # Si la eleccion de la computadora es "paper", usuario gana
     else:
-        # Imprimir "User wins!"
         print("User wins!\n")
-
-# 9. Fin
