837f944b7f7cb790b70ed5a418e46505c1f85309
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-04-08 20:55:26 -0500

    Se agrega algoritmo para juego de piedra, papel o tijeras

---
+++ src/rock_paper_scissors.py
@@ -0,0 +1,76 @@
+'''
+NAME
+    rock_paper_scissors
+
+VERSION
+    0.1
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    
+
+DESCRIPTION
+    Juego "piedra, papel o tijeras" entre computadora y usuario
+    Se pide eleccion al usuario y computadora elige aleatoriamente
+
+CATEGORY
+    Game
+
+USAGE
+    py src/rock_paper_scissors.py
+
+ARGUMENTS
+    None
+
+INPUT
+    Eleccion del usuario entre "rock", "paper" o "scissors"
+
+OUTPUT
+    Opciones elegidas y notificacion sobre ganador del juego
+
+SEE ALSO
+    None
+
+'''
+
+# 1. Inicio
+
+# 2. Definir opciones validas
+
+# 3. Leer eleccion del usuario desde teclado
+
+# 4. Estandarizar formato del texto de la eleccion del usuario
+
+# 5. Si la eleccion del usuario no es valida, entonces
+    # Imprimir mensaje de error y terminar el programa
+
+# 6. Calcular eleccion de la computadora
+
+# 7. Imprimir eleccion de usuario y de computadora
+
+# 8. Comparar eleccion del usuario con eleccion de la computadora
+
+# 8.1. Si la eleccion del usuario y de la computadora son iguales
+    # Imprimir "Tie"
+
+# 8.2. Si la eleccion del usuario es "rock", entonces
+    # 8.2.1. Si la eleccion de la computadora es "paper", entonces
+        # Imprimir "Computer wins!"
+    # 8.2.2. Si la eleccion de la computadora es "scissors", entonces
+        # Imprimir "User wins!"
+
+# 8.3. Si la eleccion del usuario es "paper", entonces
+    # 8.3.1. Si la eleccion de la computadora es "rock", entonces
+        # Imprimir "User wins!"
+    # 8.3.2. Si la eleccion de la computadora es "scissors", entonces
+        # Imprimir "Computer wins!"
+
+# 8.4. Si la eleccion del usuario es "scissors", entonces
+    # 8.4.1. Si la eleccion de la computadora es "rock", entonces
+        # Imprimir "Computer wins!"
+    # 8.4.2. Si la eleccion de la computadora es "paper", entonces
+        # Imprimir "User wins!"
+
+# 9. Fin
