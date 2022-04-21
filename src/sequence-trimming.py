128871e03f1aca91fb8c4e4488b81d81897bb10f
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-04-21 13:37:31 -0500

    Se agrega manejo de excepciones (IOError)

--- src/sequence-trimming.py
+++ src/sequence-trimming.py
@@ -37,17 +37,23 @@ SEE ALSO
 
 '''
 
-# Abrir archivo, guardar secuencias en una lista y cerrar archivo
-with open("data/4_input_adapters.txt", 'r') as sequences_file:
-    sequences_list = sequences_file.readlines()
+# Intentar ejecutar el programa
+try:
+    # Abrir archivo, guardar secuencias en una lista y cerrar archivo
+    with open("data/4_input_adapters.txt", 'r') as sequences_file:
+        sequences_list = sequences_file.readlines()
 
-# Abrir nuevo archivo, escribir cada secuencia sin adaptadores y 
-# cerrar archivo
-with open("results/4_output_no_adapters.txt", 'a') as no_adapters_file:
-    for sequence in sequences_list:
-        no_adapters_sequence = sequence[14:]
-        no_adapters_file.write(no_adapters_sequence)
+    # Abrir nuevo archivo, escribir cada secuencia sin adaptadores y 
+    # cerrar archivo
+    with open("results/4_output_no_adapters.txt", 'a') as no_adapters_file:
+        for sequence in sequences_list:
+            no_adapters_sequence = sequence[14:]
+            no_adapters_file.write(no_adapters_sequence)
 
-# Informar al usuario que el archivo sin adaptadores se ha creado
-print("\nSe ha generado el archivo 4_output_no_adapters.txt \
-      \nEl archivo se encuentra disponible en la carpeta results/ \n")
+    # Informar al usuario que el archivo sin adaptadores se ha creado
+    print("\nSe ha generado el archivo 4_output_no_adapters.txt \
+          \nEl archivo se encuentra disponible en la carpeta results/ \n")
+
+# Si no se encuentra el archivo data/4_input_adapters.txt, notificarlo
+except IOError as io_error:
+    print(f"\nEl archivo {io_error.filename} no se encuentra.\n")
