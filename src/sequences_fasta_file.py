abd2e2f9df415b8f932dd9943c7d427275550029
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-04-25 20:28:00 -0500

    Se agrega manejo de excepciones (IOError)

--- src/sequences_fasta_file.py
+++ src/sequences_fasta_file.py
@@ -3,7 +3,7 @@ NAME
     sequences_fasta_file
 
 VERSION
-    2.1
+    3.0
 
 AUTHOR
     Gabriel Ramirez Vilchis
@@ -37,21 +37,29 @@ SEE ALSO
 
 '''
 
-# Abrir archivo, guardar las lineas en una lista y cerrar archivo
-with open("data/dna_sequences.txt", 'r') as sequences_file:
-    file_lines = sequences_file.readlines()
+# Intentar leer archivo con secuencias
+try:
+    # Abrir archivo, guardar las lineas en una lista y cerrar archivo
+    with open("data/dna_sequences.txt", 'r') as sequences_file:
+        file_lines = sequences_file.readlines()
 
-# Abrir nuevo archivo FASTA, escribir secuencias y cerrar archivo
-with open("results/dna_sequences.fasta", 'w') as fasta_file:
-    # Para cada linea en la lista, crear una lista con el ID y la 
-    # secuencia, estandarizar la secuencia y escribirla en formato FASTA
-    for line in file_lines:
-        line_elements = line.split("   ")
-        seq_id = line_elements[0]
-        sequence = line_elements[1]
-        standard_sequence = sequence.upper().replace('-', '')
-        fasta_file.write(f"> {seq_id}\n{standard_sequence}")
+# Si no se encuentra el archivo con secuencias, notificarlo
+except IOError as io_error:
+    print(f"\nEl archivo {io_error.filename} no se encuentra.\n")
 
-# Informar al usuario que el archivo FASTA se ha creado
-print("\nSe ha generado el archivo dna_sequences.fasta \
-      \nEl archivo se encuentra disponible en la carpeta results/ \n")
+# Si se encuentra el archivo con secuencias, formatear a FASTA
+else:
+    # Abrir nuevo archivo FASTA, escribir secuencias y cerrar archivo
+    with open("results/dna_sequences.fasta", 'w') as fasta_file:
+        # Para cada linea en la lista, crear una lista con el ID y la 
+        # secuencia, estandarizar la secuencia y formatarla a FASTA
+        for line in file_lines:
+            line_elements = line.split("   ")
+            seq_id = line_elements[0]
+            sequence = line_elements[1]
+            standard_sequence = sequence.upper().replace('-', '')
+            fasta_file.write(f"> {seq_id}\n{standard_sequence}")
+
+    # Informar al usuario que el archivo FASTA se ha creado
+    print("\nSe ha generado el archivo dna_sequences.fasta \
+        \nEl archivo se encuentra disponible en la carpeta results/ \n")
