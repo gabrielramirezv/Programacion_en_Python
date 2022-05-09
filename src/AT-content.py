4b64c3b9e52f42819f04b007cd65b4445a9df8ca
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-05-09 16:13:21 -0500

    Se agrega pseudocódigo para el cálculo del contenido de AT

---
+++ src/AT-content.py
@@ -0,0 +1,58 @@
+'''
+NAME
+    AT-content
+
+VERSION
+    0.1
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    
+
+DESCRIPTION
+    Calcula el contenido de AT en una secuencia de DNA
+
+CATEGORY
+    DNA sequence
+
+USAGE
+    py src/AT_GC_percentage --input [PATH] [OPTIONS]
+
+ARGUMENTS
+    -i, --input     argumento necesario, lee la ruta del archivo con 
+                    secuencia de DNA
+                
+
+OUTPUT
+    Porcentaje de AT en la secuencia de DNA
+
+SEE ALSO
+    AT_GC_percentage
+
+'''
+
+# Definir error para bases no validas introducidas por el usuario
+
+# Leer desde teclado la ruta y nombre del archivo con la secuencia
+
+# Intentar leer el archivo de secuencia
+    
+    # Si hay un caracter distinto a las cuatro bases aceptadas, 
+    # generar un error InvalidBaseError()
+        
+# Si no se encuentra el archivo de secuencia, notificarlo
+
+# Si hay una base no valida, notificarlo al usuario
+
+# Si se encuentra archivo de secuencia y las bases son validas
+
+    # Eliminar caracteres ajenos a la secuencia de DNA
+
+    # Contar A, T y la longitud de la secuencia
+
+    # Calcular porcentaje de AT en la secuencia de DNA
+
+    # Imprimir resultados
+    
