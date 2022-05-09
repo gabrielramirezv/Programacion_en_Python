2fe37a49483ff5c03bc0c17998b5f729c8a2aa02
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-05-09 16:26:40 -0500

    Se agrega código funcional para cálculo del contenido de AT

--- src/AT-content.py
+++ src/AT-content.py
@@ -3,7 +3,7 @@ NAME
     AT-content
 
 VERSION
-    0.1
+    1.0
 
 AUTHOR
     Gabriel Ramirez Vilchis
@@ -33,26 +33,53 @@ SEE ALSO
 
 '''
 
-# Definir error para bases no validas introducidas por el usuario
+import re
+
+# Definir errores para bases no validas introducidas por el usuario
+class InvalidBaseError(Exception):
+    pass
+class AmbiguousBaseError(Exception):
+    pass
 
 # Leer desde teclado la ruta y nombre del archivo con la secuencia
+dna_file_name = input("\nArchivo de secuencia: ")
 
 # Intentar leer el archivo de secuencia
-    
+try:
+    # Abrir el archivo, leer el contenido y cerrar el archivo
+    with open(dna_file_name, 'r') as dna_file:
+        dna_sequence = dna_file.read()
+        
     # Si hay un caracter distinto a las cuatro bases aceptadas, 
-    # generar un error InvalidBaseError()
+    # generar un error
+    if re.search("[^ATCGN]", dna_sequence):
+        raise InvalidBaseError(f"{dna_file_name} no contiene secuencia valida")
+        
+    if dna_sequence.count("N") > 0:
+        raise AmbiguousBaseError(f"La secuencia contiene {dna_sequence.count('N')} N's")
         
 # Si no se encuentra el archivo de secuencia, notificarlo
+except IOError as io_error:
+    print(f"\nEl archivo {io_error.filename} no se encuentra.\n")
 
 # Si hay una base no valida, notificarlo al usuario
+except InvalidBaseError as invalid_base_error:
+    print(invalid_base_error.args[0] 
+          + ". Las bases aceptadas son A, T, C y G.\n")
 
 # Si se encuentra archivo de secuencia y las bases son validas
-
+else:
     # Eliminar caracteres ajenos a la secuencia de DNA
+    dna_sequence.rstrip('\n')
 
     # Contar A, T y la longitud de la secuencia
+    a_count = dna_sequence.count('A')
+    t_count = dna_sequence.count('T')
+    dna_length = len(dna_sequence)
 
     # Calcular porcentaje de AT en la secuencia de DNA
+    at_percentage = ((a_count + t_count) / dna_length) * 100
 
     # Imprimir resultados
-    
+    print(f"\nLa secuencia de DNA es {dna_sequence}\
+    \n Porcentaje de AT y GC")
