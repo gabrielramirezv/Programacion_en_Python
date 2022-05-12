a851351560c15e99564b9f6c39354c88be715505
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-05-11 19:25:57 -0500

    Se agrega paso de argumentos

--- src/AT_GC_percentage.py
+++ src/AT_GC_percentage.py
@@ -3,7 +3,7 @@ NAME
     AT_GC_percentage
 
 VERSION
-    4.0
+    5.0
 
 AUTHOR
     Gabriel Ramirez Vilchis
@@ -37,24 +37,54 @@ SEE ALSO
 '''
 
 import re
+import argparse
 
-# Definir error para bases no validas introducidas por el usuario
+# Definir errores para bases no validas introducidas por el usuario
 class InvalidBaseError(Exception):
     pass
+class AmbiguousBaseError(Exception):
+    pass
 
-# Leer desde teclado la ruta y nombre del archivo con la secuencia
-dna_file_name = input("\nArchivo de secuencia: ")
+# Crear el parser
+parser = argparse.ArgumentParser(description="Script que calcula el "
+        + "contenido de AT y GC a partir de la ruta del archivo que "
+        + "contiene una secuencia de DNA.")
+
+# Agregar y almacenar los argumentos
+parser.add_argument("-i", "--input",
+                    metavar="path/to/input/file",
+                    help="File with gene sequences",
+                    required=True)
+
+parser.add_argument("-o", "--output",
+                    metavar="path/to/output/file",
+                    help="Path for the output file",
+                    required=False)
+                    
+parser.add_argument("-r", "--round",
+                    help="Number of digits to round",
+                    type=int,
+                    required=False)
+  
+args = parser.parse_args()
+
+# Leer la ruta y nombre del archivo con la secuencia
+dna_file_name = args.input
 
 # Intentar leer el archivo de secuencia
 try:
     # Abrir el archivo, leer el contenido y cerrar el archivo
     with open(dna_file_name, 'r') as dna_file:
-        dna_sequence = dna_file.read()
+        dna_sequence = str(dna_file.read().replace('\n', ''))
     
     # Si hay un caracter distinto a las cuatro bases aceptadas, 
-    # generar un error InvalidBaseError()
-    if re.search("[^ATCG]", dna_sequence):
+    # generar un error
+    if re.search("[^ATCGN]", dna_sequence):
         raise InvalidBaseError(f"{dna_file_name} no contiene secuencia valida")
+        
+    if dna_sequence.count("N") > 0:
+        N_count = dna_sequence.count("N")
+        raise AmbiguousBaseError(f"La secuencia contiene {N_count} N's")
         
 # Si no se encuentra el archivo de secuencia, notificarlo
 except IOError as io_error:
@@ -64,6 +94,9 @@ except IOError as io_error:
 except InvalidBaseError as invalid_base_error:
     print(invalid_base_error.args[0] 
           + ". Las bases aceptadas son A, T, C y G.\n")
+        
+except AmbiguousBaseError as ambiguous_base_error:
+    print("\nSecuencia ambigua. " + ambiguous_base_error.args[0] + '\n\n')
 
 # Si se encuentra archivo de secuencia y las bases son validas, contar
 else:
@@ -81,8 +114,25 @@ else:
     at_percentage = ((a_count + t_count) / dna_length) * 100
     gc_percentage = ((g_count + c_count) / dna_length) * 100
 
+    # Si se especifica el numero de decimales, redondear
+    if args.round:
+        at_percentage = round(at_percentage, args.round)
+        gc_percentage = round(gc_percentage, args.round)
+
     # Imprimir resultados
-    print(f"\nLa secuencia de DNA es {dna_sequence}\
-        \n Porcentaje de AT y GC\
-        \n AT: {at_percentage} %\
-        \n GC: {gc_percentage} %\n")
+    if args.output:
+        with open(args.output, 'w') as output_file:
+            print(f"La secuencia de DNA es {dna_sequence}\
+                  \nSecuencia obtenida del archivo {dna_file_name} \
+                  \n\n Porcentaje de AT: {at_percentage} % \
+                  \n Porcentaje de GC: {gc_percentage} %",
+                  file=output_file)
+                  
+        print(f"\nSe ha generado el archivo {args.output}"
+              + " con el contenido de AT\n\n")
+        
+    else:
+        print(f"\nLa secuencia de DNA es {dna_sequence}\
+              \nSecuencia obtenida del archivo {dna_file_name} \
+              \n\n Porcentaje de AT: {at_percentage} %\n\n")
+    
