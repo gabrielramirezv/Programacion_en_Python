c1c42ec2c44b3a8bd2d8715fdabaa7f67d83e933
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-05-13 17:28:27 -0500

    Se agregan pruebas de la función (assert)

--- src/amino_acid_content.py
+++ src/amino_acid_content.py
@@ -3,13 +3,13 @@ NAME
     amino_acid_content
 
 VERSION
-    0.1
+    0.2
 
 AUTHOR
     Gabriel Ramirez Vilchis
 
 GITHUB
-    
+    https://github.com/gabrielramirezv/Programacion_en_Python/blob/master/src/amino_acid_content.py
 
 DESCRIPTION
     Calcula el porcentaje de un aminoacido en una secuencia proteica
@@ -28,21 +28,46 @@ SEE ALSO
 
 '''
 
-## 1. Inicio de la funcion
 def get_aa_percentage(protein_sequence, amino_acid):
     
-## 2. Estandarizar a mayusculas los parametros que recibe la funcion
+    # Estandarizar a mayusculas los parametros que recibe la funcion
     protein_sequence = protein_sequence.upper()
     amino_acid = amino_acid.upper()
     
-## 3. Contar las veces que aparece el aminoacido en la secuencia proteica
+    # Contar las veces que aparece el aminoacido en la secuencia proteica
     amino_acid_count = protein_sequence.count(amino_acid)
     
-## 4. Calcular la longitud de la secuencia
+    # Calcular la longitud de la secuencia
     protein_sequence_length = len(protein_sequence)
     
-## 5. Calcular el porcentaje del aminoacido dentro de la secuencia
+    # Calcular el porcentaje del aminoacido dentro de la secuencia
     amino_acid_content = (amino_acid_count / protein_sequence_length) * 100
     
-## 6. Fin de la funcion (devolver el porcentaje del aminoacido en la secuencia)
+    # Devolver el porcentaje del aminoacido en la secuencia
     return amino_acid_content
+    
+
+# Hacer pruebas
+try:
+    assert get_aa_percentage("MSRSLLLRFLLFLLLLPPLP", 'M') == 5, \
+        "Parece que el calculo del porcentaje es incorrecto. \
+         \nError en assert get_aa_percentage(\"MSRSLLLRFLLFLLLLPPLP\", 'M') == 5"
+    
+    assert get_aa_percentage("MSRSLLLRFLLFLLLLPPLP", 'r') == 10, \
+        "Parece que no se aceptan aminoacidos en minusculas. \
+         \nError en assert get_aa_percentage(\"MSRSLLLRFLLFLLLLPPLP\", 'r') == 10"
+    
+    assert get_aa_percentage("msrslllrfllfllllpplp", 'L') == 50, \
+        "Parece que no se aceptan secuencias en minusculas. \
+         \nError en assert get_aa_percentage(\"msrslllrfllfllllpplp\", 'L') == 50"
+         
+    assert get_aa_percentage("MSRSLLLRFLLFLLLLPPLP", 'Y') == 0, \
+        "Parece que se estan contando aminoacidos ausentes. \
+         \nError en assert get_aa_percentage(\"MSRSLLLRFLLFLLLLPPLP\", 'Y') == 0"
+    
+# Informar si la funcion esta cumpliendo su proposito adecuadamente o reportar errores
+except AssertionError as assertion_error:
+    print("\nAlgo salio mal.\n"+ str(assertion_error) + "\n") 
+       
+else:
+    print("\nTodo salio bien.\n\n")
