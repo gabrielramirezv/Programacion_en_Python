8c8cace3435e0c13c737527f15d1512a1bac3589
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-05-13 15:04:08 -0500

    Se agrega código de la función para calcular el contenido de aminoácidos

--- src/amino_acid_content.py
+++ src/amino_acid_content.py
@@ -29,13 +29,20 @@ SEE ALSO
 '''
 
 ## 1. Inicio de la funcion
-
+def get_aa_percentage(protein_sequence, amino_acid):
+    
 ## 2. Estandarizar a mayusculas los parametros que recibe la funcion
-
+    protein_sequence = protein_sequence.upper()
+    amino_acid = amino_acid.upper()
+    
 ## 3. Contar las veces que aparece el aminoacido en la secuencia proteica
-
+    amino_acid_count = protein_sequence.count(amino_acid)
+    
 ## 4. Calcular la longitud de la secuencia
-
+    protein_sequence_length = len(protein_sequence)
+    
 ## 5. Calcular el porcentaje del aminoacido dentro de la secuencia
-
+    amino_acid_content = (amino_acid_count / protein_sequence_length) * 100
+    
 ## 6. Fin de la funcion (devolver el porcentaje del aminoacido en la secuencia)
+    return amino_acid_content
