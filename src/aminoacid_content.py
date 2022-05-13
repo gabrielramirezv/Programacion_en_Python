6159abb21bc59b9efd5e33624f489df0b975181e
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-05-13 14:44:03 -0500

    Se agrega algoritmo para el cálculo de contenido de aminoácidos en secuencias proteicas

---
+++ src/aminoacid_content.py
@@ -0,0 +1,41 @@
+'''
+NAME
+    aminoacids-content
+
+VERSION
+    0.1
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    
+
+DESCRIPTION
+    Calcula el porcentaje de un aminoacido en una secuencia proteica
+
+CATEGORY
+    Protein sequence
+
+USAGE
+    py aminoacid_content.py
+
+ARGUMENTS
+    
+
+SEE ALSO
+    AT_GC_percentage
+
+'''
+
+## 1. Inicio de la funcion
+
+## 2. Estandarizar a mayusculas los parametros que recibe la funcion
+
+## 3. Contar las veces que aparece el aminoacido en la secuencia proteica
+
+## 4. Calcular la longitud de la secuencia
+
+## 5. Calcular el porcentaje del aminoacido dentro de la secuencia
+
+## 6. Fin de la funcion (devolver el porcentaje del aminoacido en la secuencia)
