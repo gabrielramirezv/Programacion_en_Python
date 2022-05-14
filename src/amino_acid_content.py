83a1604d81cd96921b41e636360f2b3533047052
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-05-13 19:30:36 -0500

    Se agrega código funcional para el cálculo del porcentaje de una lista de aminoácidos

--- src/amino_acid_content.py
+++ src/amino_acid_content.py
@@ -3,7 +3,7 @@ NAME
     amino_acid_content
 
 VERSION
-    1.1
+    2.0
 
 AUTHOR
     Gabriel Ramirez Vilchis
@@ -12,21 +12,22 @@ GITHUB
     https://github.com/gabrielramirezv/Programacion_en_Python/blob/master/src/amino_acid_content.py
 
 DESCRIPTION
-    Calcula el porcentaje de un aminoacido en una secuencia proteica
+    Calcula el porcentaje de una lista de aminoacidos en una secuencia 
+    proteica
 
 CATEGORY
     Protein sequence
 
 USAGE
-    py amino_acid_content.py [-h] -s SEQUENCE -a AMINOACID 
+    py amino_acid_content.py [-h] -s SEQUENCE [-a AMINOACIDS] 
         [-o path/to/output/file] [-r ROUND]
 
 ARGUMENTS
     -h, --help            show this help message and exit
     -s SEQUENCE, --sequence SEQUENCE
                         Protein sequence to analyze
-    -a AMINOACID, --aminoacid AMINOACID
-                        Amino acid to search (single-letter code)
+    -a AMINOACIDS, --aminoacids AMINOACIDS
+                        Amino acids to search (single-letter code)
     -o path/to/output/file, --output path/to/output/file
                         Path for the output file
     -r ROUND, --round ROUND
@@ -37,52 +38,95 @@ SEE ALSO
 
 '''
 
+import argparse
+
 def get_aa_percentage(
         protein_sequence, 
         amino_acids_list=['A','I','L','M','F','W','Y','V']):
     
-    # Estandarizar a mayusculas los parametros que recibe la funcion
+    # Estandarizar la secuencia a mayusculas
     protein_sequence = protein_sequence.upper()
     
+    # Inicializar el porcentaje acumulado a cero
     aa_acumulated_percentage = 0
     
+    # Calcular el porcentaje para cada aminoacido y acumularlo
     for amino_acid in amino_acids_list:
         
+        # Estandarizar el aminoacido a mayusculas y contar sus 
+        # repeticiones en la secuencia
         amino_acid = amino_acid.upper()
-        
-        # Contar las veces que aparece el aminoacido en la secuencia proteica
         amino_acid_count = protein_sequence.count(amino_acid)
         
         # Calcular la longitud de la secuencia
         protein_sequence_length = len(protein_sequence)
         
         # Calcular el porcentaje del aminoacido dentro de la secuencia
-        amino_acid_percentage = (amino_acid_count / protein_sequence_length) * 100
+        amino_acid_percentage = (amino_acid_count 
+                                 / protein_sequence_length) * 100
         
+        # Acumular el porcentaje
         aa_acumulated_percentage += amino_acid_percentage
         
     # Devolver el porcentaje del aminoacido en la secuencia
     return aa_acumulated_percentage
 
 
-# Hacer pruebas
-try:
-    assert get_aa_percentage("MSRSLLLRFLLFLLLLPPLP", ["M"]) == 5, \
-        "Hubo un error en assert get_aa_percentage(\"MSRSLLLRFLLFLLLLPPLP\", [\"M\"]) == 5"
-        
-    assert get_aa_percentage("MSRSLLLRFLLFLLLLPPLP", ['M', 'L']) == 55, \
-        "Hubo un error en assert get_aa_percentage(\"MSRSLLLRFLLFLLLLPPLP\", ['M', 'L']) == 55"
-        
-    assert get_aa_percentage("MSRSLLLRFLLFLLLLPPLP", ['F', 'S', 'L']) == 70, \
-        "Hubo un error en assert get_aa_percentage(\"MSRSLLLRFLLFLLLLPPLP\", ['F', 'S', 'L']) == 70"
-        
-    assert get_aa_percentage("MSRSLLLRFLLFLLLLPPLP") == 65, \
-        "Hubo un error en assert get_aa_percentage(\"MSRSLLLRFLLFLLLLPPLP\") == 65"
+# Crear el parser
+parser = argparse.ArgumentParser(description="Script que calcula el "
+        + "contenido de un aminoacido en una secuencia proteica.")
 
-# Informar si la funcion esta cumpliendo su proposito adecuadamente o reportar errores
-except AssertionError as assertion_error:
-    print("\nAlgo salio mal.\n"+ str(assertion_error) + "\n") 
+# Agregar y almacenar los argumentos
+parser.add_argument("-s", "--sequence",
+                    help="Protein sequence to analyze",
+                    type=str,
+                    required=True)
+                    
+parser.add_argument("-a", "--aminoacids",
+                    help="Amino acids to search (single-letter code)",
+                    type=list,
+                    required=False)
 
+parser.add_argument("-o", "--output",
+                    metavar="path/to/output/file",
+                    help="Path for the output file",
+                    required=False)
+                    
+parser.add_argument("-r", "--round",
+                    help="Number of digits to round",
+                    type=int,
+                    required=False)
+  
+args = parser.parse_args()
+
+
+# Recibir secuencia y aminoacido como argumentos desde linea de comandos
+protein_sequence = args.sequence
+amino_acids_list = args.aminoacids
+
+# Obtener el contenido de aminoacido en la secuencia
+amino_acid_content = get_aa_percentage(protein_sequence, amino_acids_list)
+
+# Redondear el porcentaje si fue solicitado por el usuario
+if args.round:
+    amino_acid_content = round(amino_acid_content, args.round)
+
+# Imprimir el porcentaje en el archivo indicado por el usuario
+if args.output:
+        with open(args.output, 'w') as output_file:
+            print(f"Secuencia proteica: {protein_sequence} \
+                \nAminoacidos: {amino_acids_list} \
+                \n\n Contenido de {amino_acids_list} en la secuencia:"
+                + f" {amino_acid_content} %",
+                  file=output_file)
+                  
+        print(f"\nSe ha generado el archivo {args.output}"
+              + f" con el contenido de {amino_acids_list}.\n\n")
+
+# Imprimir resultado a pantalla si no fue solicitado un archivo output
 else:
-    print("\nTodo salio bien.\n\n")
+    print(f"\nSecuencia proteica: {protein_sequence} \
+        \nAminoacidos: {amino_acids_list} \
+        \n\n Contenido de {amino_acids_list} en la secuencia:"
+        + f" {amino_acid_content} %\n\n")
     
