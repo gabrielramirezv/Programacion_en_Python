fb079dab8568bb4bab5a7f732c376b3638801e1e
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2022-05-31 13:13:55 -0500

    Se agrega módulo para manejo de secuencias de DNA

---
+++ src/DNAtools.py
@@ -0,0 +1,35 @@
+'''
+NAME
+    DNAtools
+
+VERSION
+    1.0
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    
+
+DESCRIPTION
+    Herramientas para manejo de secuencias de DNA
+
+CATEGORY
+    DNA modules
+
+USAGES
+    import DNAtools
+
+ARGUMENTS
+    None
+
+SEE ALSO
+    FASTAtools
+
+'''
+
+# Funcion para conteo de nucleotidos
+
+# Funcion para calculo de porcentaje de AT y GC
+
+# Funcion para eliminar adaptadores de una secuencia
